1b46e473d287e1bae450598f3663dfc350a8805d
ViaanROM <viaan152013@gmail.com>
2026-03-03 17:42:25 +0530

    TB-X505X: Move prebuilt kernel to kernel/lenovo path
    Relocate prebuilt Image and dtbo.img from device prebuilts
    directory to kernel/lenovo/TB-X505X.
    
    Update BoardConfig.mk to reflect new kernel path and
    remove legacy prebuilts logic.
    
    No functional changes.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,7 +9,6 @@ DEVICE_PATH := device/lenovo/TB-X505X
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := generic
 
@@ -32,21 +31,18 @@ BOARD_BOOT_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78B0000 firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true loop.max_part=7 printk.devkmsg=on buildvariant=user
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_KERNEL_IMAGE_NAME := Image
-BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
-# Kernel - prebuilt
+# Prebuilt kernel (moved to kernel/lenovo)
 TARGET_FORCE_PREBUILT_KERNEL := true
-ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilts/kernel
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/dtbo.img
-BOARD_KERNEL_SEPARATED_DTBO := 
-endif
+TARGET_PREBUILT_KERNEL := kernel/lenovo/TB-X505X/Image
+BOARD_PREBUILT_DTBOIMAGE := kernel/lenovo/TB-X505X/dtbo.img
+BOARD_KERNEL_SEPARATED_DTBO := true
 
 # Partitions
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
 BOARD_DTBOIMG_PARTITION_SIZE := 8388608
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
@@ -71,14 +67,9 @@ VENDOR_SECURITY_PATCH := 2021-12-05
 
 # Verified Boot
 BOARD_AVB_ENABLE := false
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
-BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
-BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
-BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
-BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
-# Inherit the proprietary files
+# Inherit proprietary files
 include vendor/lenovo/TB-X505X/BoardConfigVendor.mk
