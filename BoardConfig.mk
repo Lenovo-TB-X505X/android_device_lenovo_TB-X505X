1e56dac90aa15f3383cc72e1a028b92193406f82
Viaan <viaan152013@gmail.com>
2026-04-04 22:05:24 +0530

    TB-X505X: Move props to configs/props
    Refactor property handling by relocating props into
    a dedicated configs/props directory for improved
    structure and easier maintenance.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,10 +51,10 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 TARGET_BOARD_PLATFORM := msm8937
 
 # Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
